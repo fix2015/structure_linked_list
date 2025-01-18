481887c79fbf6e2c42d088672b9ef7132039f8d0
Semianchuk Vitalii <fix20152@gmail.com>
2025-01-18 13:49:11 +0000

    Create task file

---
+++ index.js
@@ -0,0 +1,31 @@
+class Node {
+    constructor(data) {
+       
+    }
+}
+
+class LinkedList {
+    constructor() {
+       
+    }
+
+    // Insert a new node at the end of the list
+    insert(data) {
+       
+    }
+
+    // Delete the first node containing the given data
+    delete(data) {
+      
+    }
+
+    // Search for a node by its data
+    search(data) {
+     
+    }
+
+    // Traverse the list and print the data of each node
+    traverse() {
+        
+    }
+}
